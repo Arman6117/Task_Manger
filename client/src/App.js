781245052d6c516967c302a48560b47b8c1467b0
Arman Patel <vanixgaming634@gmail.com>
2023-05-29 11:47:09 +0530

    Intial commit

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Container from './Components/Container'
+const App = () => {
+  return (
+    <div><Container/></div>
+  )
+}
+
+export default App
---
+++ client/src/Components/Container.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Container.css'
+const Container = () => {
+  return (
+    <div className='main'>
+        <div className='title'>
+            <span>Hello <span>Arman</span></span>
+            <span>Here's what you <br/> need to do</span>
+            <div className='tasks'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Container
